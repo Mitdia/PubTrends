5b2c0e3851a05e708946373a57c57c2328fc2e3c
Dmitry Knorre <knorre.dmitry.junior@gmail.com>
2025-04-02 20:13:35 +0200

    Added a function to convert a list of GEO ids to a summary dictionaries

--- EUtilsAPI.py
+++ EUtilsAPI.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Dict
 import requests
 import xml.etree.ElementTree as ET
 
@@ -18,3 +18,23 @@ def get_relevant_geo_ids(pmids_list : List[str] = None) -> List[str]:
         geo_id = link.find("Id").text
         geo_ids.append(geo_id)
     return geo_ids
+
+
+def geo_ids_to_summaries(geo_ids_list : List[str]) -> Dict[str, Dict[str, str]]:
+    
+    geo_query = ",".join(geo_ids_list)
+    default_api_summary = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi"
+    params = {
+        "db": "gds",
+        "id": geo_query,
+    }
+    response = requests.get(default_api_summary, params=params)
+    root = ET.fromstring(response.text)
+    geo_ids_params = {}
+    for geo_id_entry in root.findall(".//DocSum"): 
+        geo_id = geo_id_entry.find("Id").text
+        geo_id_param = {}
+        for child in geo_id_entry.findall(".//Item"):
+            geo_id_param[child.attrib["Name"]] = child.text
+        geo_ids_params[geo_id] = geo_id_param
+    return geo_ids_params
