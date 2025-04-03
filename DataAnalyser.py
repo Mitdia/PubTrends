4fa87de443a6f19fafbf54de27b16392cfd6aa5a
Dmitry Knorre <knorre.dmitry.junior@gmail.com>
2025-04-03 19:11:10 +0200

    Added function to embed the vector representation into 2d space using TSNE

--- DataAnalyser.py
+++ DataAnalyser.py
@@ -1,5 +1,8 @@
-from typing import List, Dict
+from typing import Dict
+import numpy as np
+from scipy.sparse import csr_matrix
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.manifold import TSNE
 
 
 def text_to_vector(text_dict: Dict[str, str]):
@@ -9,4 +12,11 @@ def text_to_vector(text_dict: Dict[str, str]):
     return feature_matrix
 
 
+def embed_in_2d(matrix : csr_matrix, perplexity : int = 30) -> np.ndarray:
+    tsne = TSNE(
+        n_components=2,
+        perplexity=perplexity,
+        init='random', 
+    )
+    return tsne.fit_transform(matrix)
 
