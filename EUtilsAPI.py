864f94c4a1c99b1b0cb42ab48c6b39e4f5039e01
Dmitry Knorre <knorre.dmitry.junior@gmail.com>
2025-04-03 18:37:01 +0200

    Added a function to convert summaries to text  with given fields. Additionally added function to scrape the relevant website for the "overall design" information

--- EUtilsAPI.py
+++ EUtilsAPI.py
@@ -1,6 +1,7 @@
 from typing import List, Dict
 import requests
 import xml.etree.ElementTree as ET
+from bs4 import BeautifulSoup
 
 
 def get_relevant_geo_ids(pmids_list : List[str] = None) -> List[str]:
@@ -38,3 +39,30 @@ def geo_ids_to_summaries(geo_ids_list : List[str]) -> Dict[str, Dict[str, str]]:
             geo_id_param[child.attrib["Name"]] = child.text
         geo_ids_params[geo_id] = geo_id_param
     return geo_ids_params
+
+
+def scrape_overall_design_by_gse(gse_id: str) -> str:
+    url = f"https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc={gse_id}"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    try: 
+        overall_design = soup.find(string="Overall design").find_next().text
+    except AttributeError:
+        overall_design = ""
+        print(gse_id, "- Fail!")
+        print(soup.find(string="Overall design"))
+    return overall_design
+
+
+def summary_to_document(summaries : Dict[str, Dict[str, str]], fields : List[str] = None) -> Dict[str, str]:
+    if fields is None:
+        fields = ["title", "gdsType", "summary", "taxon"]
+    documents = {}
+    for geo_id, summary in summaries.items():
+        document = ""
+        for field_name in fields:
+            document += field_name + ": " + summary[field_name] + "\n"
+        overall_design = scrape_overall_design_by_gse("GSE" + summary["GSE"])
+        document += "Overall design: " + overall_design
+        documents[geo_id] = document
+    return documents
