442456d40c7e3a43d566ab34df91d8f38f699e1c
Dmitry Knorre <knorre.dmitry.junior@gmail.com>
2025-04-03 21:05:43 +0200

    Added a function to process a list of pmids and get all the relevant info out of it

--- DataAnalyser.py
+++ DataAnalyser.py
@@ -1,9 +1,12 @@
 from typing import Dict, List
 import numpy as np
 from scipy.sparse import csr_matrix
+
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.manifold import TSNE
 from sklearn.cluster import KMeans
+
+from EUtilsAPI import get_relevant_geo_ids, geo_ids_to_summaries, summary_to_document
 
 
 def text_to_vector(text_dict: Dict[str, str]):
@@ -26,3 +29,21 @@ def cluster(features : csr_matrix, k : int = 5) -> List[int]:
     clusterizator = KMeans(k)
     labels = clusterizator.fit_predict(features)
     return labels
+
+
+def process_pmids(pmids : List[str], k : int) -> tuple[List[str], List[int], np.ndarray, Dict[str, List[str]]]:
+    geo_ids = {}
+    for pmid in pmids:
+        geo_ids_list = get_relevant_geo_ids([pmid])
+        for geo_id in geo_ids_list:
+            if geo_id in geo_ids.keys():
+                geo_ids[geo_id].append(pmid)
+            else:
+                geo_ids[geo_id] = [pmid]
+    summaries = geo_ids_to_summaries(geo_ids.keys())
+    documents = summary_to_document(summaries)
+    feature_matrix = text_to_vector(documents)
+    labels = documents.keys()
+    clusters = cluster(feature_matrix, k)
+    embedded_features = embed_in_2d(feature_matrix)
+    return labels, clusters, embedded_features, geo_ids
